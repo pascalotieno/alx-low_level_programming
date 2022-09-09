057341af521411d033c5971226221f74c83331b4
pascalotieno <otieno2john70@gmail.com>
2022-09-09 08:14:49 -0700

    C 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,12 +14,12 @@ int main(void)
 
 	e = 'e';
 	q = 'q';
-        
+
 	for (low = 'a'; low <= 'z'; low++)
 	{
-                if  (low != e && low != q)
-		putchar(low);
-        }
+		if (low != e && low != q)
+			putchar(low);
+	}
 	putchar('\n');
 
 	return (0);
