77add70da6a87fa9ced803be5b10cbf8d796403c
pascalotieno <otieno2john70@gmail.com>
2022-09-14 05:33:39 -0700

    four t

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,11 @@
  * Return: 1 if character, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
-{	
+
+{
 	if ((c >= 'a' && c <= 'z') ||
 	    (c >= 'A' && c <= 'Z'))
-	        return (1);
+		return (1);
 	else
-	        return (0);
+		return (0);
 }
