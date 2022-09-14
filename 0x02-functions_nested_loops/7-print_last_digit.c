9c0096bd5b39415d7c208f860189534221a17431
pascalotieno <otieno2john70@gmail.com>
2022-09-14 14:16:12 -0700

    seven

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,17 @@
  * print_last_digit - Prints the last digit of a number.
  * @n: The numberin question.
  *
+ * int main(void)
  */
-int print_last_digit(int n)
+int print_last_digit (int n)
+
 {
-	int last_digit = n & 10;
+	int last_digit = n % 10;
 
 	if (last_digit < 0)
 		last_digit *= -1;
 
-	_putchar(last_digit = '0');
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
