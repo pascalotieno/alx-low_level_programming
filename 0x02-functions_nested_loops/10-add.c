11f8d84362906868baf920c248f8c4d38ba9ba6f
pascalotieno <otieno2john70@gmail.com>
2022-09-14 16:01:57 -0700

    ten t

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two intagers.
+ * @num1: The first intager to be added.
+ *@num2: The second intager to ba added.
+ *
+ *Return: The result of the addition.
+ */
+int add(int num1, int mun2)
+{
+	return (num1 + mun2);
+}
