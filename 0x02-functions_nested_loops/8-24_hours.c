c8a41449683cf5bee89788392a567f232181810b
pascalotieno <otieno2john70@gmail.com>
2022-09-14 15:28:09 -0700

    nine t

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,16 @@
 void jack_bauer(void)
 {
 	int hour, minute;
-	
+
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
+			-putchar((hour % 10) + '0');
+			_putcar(':');
 			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar((minute % 10 ) + '0');
 			_putchar('\n');
 		}
 	}
