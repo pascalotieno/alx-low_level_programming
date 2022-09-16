ab0436001651d0cd6875cbda3972c5af2db88d7e
pascalotieno <otieno2john70@gmail.com>
2022-09-16 04:15:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intagers.
+ * @a: The first intager to be multiplied.
+ * @b: The second intager to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
