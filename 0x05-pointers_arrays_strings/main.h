62ba7e0115e7821d77f1e1df9c9ea0938ae0b944
pascalotieno <otieno2john70@gmail.com>
2022-09-19 05:11:13 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef _ALX_S_
+#define _ALX_S_
+
+int _putchr(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
