bb8e3f413e643c5927f055f3c51f1c077600a526
pascalotieno <otieno2john70@gmail.com>
2022-09-28 08:28:28 -0700

    final task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -32,7 +32,7 @@ int wildcmp(char *s1, char *s2)
 			if ((wildcmp(s1, s2 + 1)) == 1)
 				return (1);
 		}
-		eturn (wildcmp(s1 + 1, s2));
+		return (wildcmp(s1 + 1, s2));
 	}
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,8 @@
 #define _ALX_H_
 
 int _putchar(char c);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
