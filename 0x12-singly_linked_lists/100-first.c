1f6fcbb9785685d8c3392c14f23d5a8a23e5f280
pascalotieno <otieno2john70@gmail.com>
2022-10-24 04:50:50 -0700

    hot fix 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+
+	return (0);
+}
